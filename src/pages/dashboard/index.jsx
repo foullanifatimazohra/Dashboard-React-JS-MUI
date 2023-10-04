eeb678b64be497f883133e30d8b97bfc8ae02547
foullanifatimazohra <foullanifatoum@gmail.com>
2023-10-04 09:29:17 +0100

    feat : complete header component

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,3 +1,11 @@
+import Header from "../../components/ui/Header";
+import { Box } from "@mui/material";
 export const Dashboard = () => {
-  return <div>index</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+      </Box>
+    </Box>
+  );
 };
